1e2c2fbcc85c12048851999b6b52b3c4196a6a80
Soenke Sobott <sobott.soenke@gmail.com>
2023-02-07 14:32:18 +0700

    PERI: Refactoring

--- src/main/java/org/soenke/sobott/entity/ArticleAvailableQuantity.java
+++ src/main/java/org/soenke/sobott/entity/ArticleAvailableQuantity.java
@@ -4,15 +4,6 @@ public class ArticleAvailableQuantity {
     private String articleNumber;
     private Integer availableQuantity;
 
-    public ArticleAvailableQuantity(String articleNumber, Integer availableQuantity) {
-        this.articleNumber = articleNumber;
-        this.availableQuantity = availableQuantity;
-    }
-
-    public ArticleAvailableQuantity() {
-        // empty
-    }
-
     public String getArticleNumber() {
         return articleNumber;
     }
